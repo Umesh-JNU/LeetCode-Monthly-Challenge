f86df23f6f39d86aecb30d874f07a0757574f7e4
Umesh Kumar <56982559+Umesh-JNU@users.noreply.github.com>
2021-05-05 11:16:48 +0530

    Update set-matrix-zeroes.cpp
    More efficient, reduced space complexity in optimized version.

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -20,3 +20,41 @@ public:
         }
     }
 };
+// optimized version
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool cell1 = false;
+        for(int i=0; i<matrix.size(); i++){
+            if(matrix[i][0]==0){
+                cell1 = true;
+            }
+            for(int j=1; j<matrix[0].size(); j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+        // columns
+        for(int r=1; r<matrix.size(); r++){
+            for(int c=1; c<matrix[0].size(); c++){
+                if(matrix[0][c]==0 || matrix[r][0]==0){
+                    matrix[r][c]=0;
+                } 
+            }
+        }
+        
+        // first row
+        if(matrix[0][0]==0){
+            for(int c=0; c<matrix[0].size(); c++){
+                matrix[0][c] = 0;
+            }
+        }
+        if(cell1){
+            for(int r=0; r<matrix.size(); r++){
+                matrix[r][0]=0;
+            }
+        }
+    }
+};
