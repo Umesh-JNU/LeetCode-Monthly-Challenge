b717be5c6344164e7ca33a83c376c8e69beb4831
Umesh Kumar <56982559+Umesh-JNU@users.noreply.github.com>
2021-05-28 23:58:56 +0530

    Update maximum-product-of-word-lengths.cpp

--- maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
+++ maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
@@ -1,3 +1,5 @@
+// brute for approach
+// time complexity = O(N^2 * l^2)
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
@@ -19,6 +21,41 @@ public:
                 }
             }
         }
+        return res;
+    }
+};
+
+// efficient approach using bit-manipulation
+// time-complexity O(N^2)
+// But it can further be optimized
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        vector<int> bitCodeVal;
+        int p;
+        for (auto word : words)
+        {
+            p = 0;
+            for (auto c : word)
+            {
+                p |= 1 << (c - 'a');
+            }
+            bitCodeVal.push_back(p);
+        }
+
+        int res = 0;
+        for (int i = 0; i < words.size() - 1; i++)
+        {
+            for (int j = i + 1; j < words.size(); j++)
+            {
+                if (!(bitCodeVal[i] & bitCodeVal[j]))
+                {
+                    int x = words[i].length() * words[j].length();
+                    res = max(res, x);
+                }
+            }
+        }
+        
         return res;
     }
 };
