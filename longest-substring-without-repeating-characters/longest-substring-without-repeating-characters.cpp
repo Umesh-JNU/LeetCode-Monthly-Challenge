e688c60a99fda52a032461c74f4f8a6bf9f955a7
Umesh Kumar <56982559+Umesh-JNU@users.noreply.github.com>
2021-08-17 14:28:19 +0530

    Time: 16 ms (61.80%), Space: 8.3 MB (56.78%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,28 +6,23 @@ public:
         if (n <= 1)
             return n;
 
-        int ans = 0;
-        map<int, int> freq;
-        int l = 0, r = 0;
+        unordered_map<char, int> mp;
+        int l = 0, r = 0, ans = 0;
         while (r < n)
-        {
-            if (freq.find(s[r]) == freq.end())
+        {   
+            //    0 1 2 3 4 5 6 7 8 9
+            // if a b c a a b c d b e 
+            //            l r
+            // mp[b] = 1
+            // Here we will not increase `` l `` pointer as l > 1 ie. start pointer is pointing to another substring 
+            if (mp.find(s[r]) != mp.end() && l <= mp[s[r]])
             {
-                freq[s[r]]++;
-                r++;
+                l = mp[s[r]] + 1;
+            }
 
-                ans = max(ans, r - l);
-            }
-            else
-            {
-                do
-                {
-                    freq[s[l]]--;
-                    if (freq[s[l]] == 0)
-                        freq.erase(s[l]);
-                    l++;
-                } while (s[l - 1] != s[r]);
-            }
+            mp[s[r]] = r;
+            r++;
+            ans = max(ans, r - l);
         }
         return ans;
     }
