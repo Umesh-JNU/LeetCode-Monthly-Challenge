9eaf9fe5bbe69ba8525e91a7889973262bc1d0b1
Umesh Kumar <56982559+Umesh-JNU@users.noreply.github.com>
2021-09-29 08:33:19 +0530

    Time: 12 ms (73.37%), Space: 8.3 MB (73.06%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s)
-    {
-        int n = s.size();
-        if (n <= 1)
-            return n;
+{
+    int n = s.size();
+    if (n == 0)
+        return 0;
+    unordered_map<char, int> mp;
+    int l = 0, ans = 0;
+    for(int r=0; r<n; r++){
+        if (mp.find(s[r]) != mp.end() && l <= mp[s[r]])
+            l = mp[s[r]] + 1;
 
-        unordered_map<char, int> mp;
-        int l = 0, r = 0, ans = 0;
-        while (r < n)
-        {   
-            //    0 1 2 3 4 5 6 7 8 9
-            // if a b c a a b c d b e 
-            //            l r
-            // mp[b] = 1
-            // Here we will not increase `` l `` pointer as l > 1 ie. start pointer is pointing to another substring 
-            if (mp.find(s[r]) != mp.end() && l <= mp[s[r]])
-            {
-                l = mp[s[r]] + 1;
-            }
-
-            mp[s[r]] = r;
-            r++;
-            ans = max(ans, r - l);
-        }
-        return ans;
+        mp[s[r]] = r;
+        ans = max(ans, r - l + 1);
     }
+    return ans;
+}
 };
