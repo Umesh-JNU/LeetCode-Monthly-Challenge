3ea8e14d02091a2f4e7bfc85501379e220e3c952
Umesh Kumar <56982559+Umesh-JNU@users.noreply.github.com>
2021-10-05 00:01:27 +0530

    Time: 735 ms (5.04%), Space: 516.4 MB (5.03%) - LeetHub

--- max-area-of-island/max-area-of-island.cpp
+++ max-area-of-island/max-area-of-island.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>> &grid, int i, int j){
-        if(i < 0 || j < 0 || i == grid.size() || j==grid[0].size() || grid[i][j]<=0)
-            return 0;
-
-        grid[i][j] = -1;
-        return 1 + dfs(grid, i - 1, j) +     // top
-                   dfs(grid, i, j - 1) +     // left
-                   dfs(grid, i + 1, j) +     // bottom
-                   dfs(grid, i, j + 1);      // right
+    bool isValid(int i, int j, int m, int n){  // Checking coordinates are valid or not
+        if(i>=0 && i<m && j>=0 && j<n) return true;
+        return false;
     }
-    int maxAreaOfIsland(vector<vector<int>> &grid)
-    {
-        int row = grid.size();
-        // base condition
-        if(row == 0)
-            return 0;
-        int col = grid[0].size();
-        int ans = 0;
-        for (int i = 0; i < row; i++){
-            for (int j = 0; j < col; j++){
-                if(grid[i][j]==1){
-                    ans = max(ans, dfs(grid, i, j));
+    void dfs(vector<vector<int>> grid, int m, int n, vector<vector<int>>& visited, int i, int j, int &currArea){
+        visited[i][j] = 1; //  Marking 1 so that next time will not visit
+		
+		// If coordinates are valid and not visited and value equal to 1 then call recursively 
+        if(isValid(i-1, j, m, n) && !visited[i-1][j] && grid[i-1][j] == 1){ // up
+            currArea++;
+            dfs(grid, m, n, visited, i-1, j, currArea);
+        }
+        if(isValid(i, j+1, m, n) && !visited[i][j+1] && grid[i][j+1] == 1){ // right
+            currArea++;
+            dfs(grid, m, n, visited, i, j+1, currArea);
+        }
+        if(isValid(i+1, j, m, n) && !visited[i+1][j] && grid[i+1][j] == 1){ // down
+            currArea++;
+            dfs(grid, m, n, visited, i+1, j, currArea);
+        }
+        if(isValid(i, j-1, m, n) && !visited[i][j-1] && grid[i][j-1] == 1){ // left
+            currArea++;
+            dfs(grid, m, n, visited, i, j-1, currArea);
+        }
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> visited(m, vector<int>(n,0));
+        int res = INT_MIN;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int currArea = 0; // Calculate area for current dfs call
+                if(!visited[i][j] && grid[i][j] == 1){
+                    currArea = 1;
+                    dfs(grid, m, n, visited, i, j, currArea); 
                 }
+                res = max(res, currArea);
             }
         }
-        return ans;
+        return res;
     }
 };
